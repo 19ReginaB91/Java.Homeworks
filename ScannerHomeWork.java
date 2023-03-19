c7e36255b5d3a9b38cd359a00ec9a74a0bcfcfc1
19ReginaB91 <126571780+19ReginaB91@users.noreply.github.com>
2023-03-20 00:37:34 +0100

    ScannerHomeWork.java
    Java Basic Home Work 2

---
+++ ScannerHomeWork.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+
+public class ScannerHomeWork {
+    public static void main(String[] args) {
+
+        // Разработайте алгоритм для считывания(через Scanner) из командной строки
+        // целочисленного числа, трансформации его в троичную систему (по аналогии
+        // как мы делали с другими системами) и вывода в командную строку результата
+
+
+
+        Scanner sc = new Scanner(System.in);
+
+//  -> /*
+        System.out.print("Enter your name:");
+        String name = sc.nextLine();
+
+
+        System.out.print("Enter your surname:");
+        String surname = sc.nextLine();
+
+
+        System.out.print("enter your age:");
+        int age = sc.nextInt();
+
+
+        System.out.print("are you student?:");
+        boolean isStudent = sc.nextBoolean();
+
+        System.out.println();
+
+        System.out.println("your full name is " + name + " " + surname);
+
+        System.out.println();
+        System.out.println();
+
+        
+
+
+       //troichnaja sistema
+
+
+        int a = 3137;
+
+        System.out.println(a);
+
+        System.out.println(3 * Math.pow(3, 3) + 1 * Math.pow(3, 2) + 3 * Math.pow(3, 1) + 7 * Math.pow(3, 0));
+        System.out.println();
+
+        long a3 = 11012012;
+
+        System.out.println(a3);
+
+
+//        System.out.println(3137 / 3);//1045
+//        System.out.println(3137 % 3);     // 2
+//        System.out.println(1045 / 3);//348
+//        System.out.println(1045 % 3);    //  1
+//        System.out.println(348 / 3);//116
+//        System.out.println(348 % 3);     //  0
+//        System.out.println(116 / 3);//38
+//        System.out.println(116 % 3);     //  2
+//        System.out.println(38 / 3);//12
+//        System.out.println(38 % 3);      //  1
+//        System.out.println(12 / 3);//4
+//        System.out.println(12 % 3);      //  0
+//        System.out.println(4 / 3);//1
+//        System.out.println(4 % 3);       //  1
+//        System.out.println(1 / 3);//0
+//        System.out.println(1 % 3);       //  1
+
+    }
+}
